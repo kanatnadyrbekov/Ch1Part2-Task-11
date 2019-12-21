4a89de5b35fa1d35a7514f69176dc7d136d58b9c
kanat nadyrbekov <kanatnadyrbekov@gmail.com>
2019-12-21 13:02:23 +0600

    Ch1Part2 Task 11

---
+++ task11.py
@@ -0,0 +1,5 @@
+# Given a string consisting of words separated by spaces. Determine how many
+# words it has. To solve the problem, use the method count.
+
+sentenses = "Hello my name is Kanat and surname is Nadyrbekov"
+print(sentenses.count(' ') + 1)
